7b5a8d9b92fb97ab83802e0f0523c90531f61665
johanarcile <johan.arcile@loria.fr>
2022-12-01 18:47:05 +0100

    last one

--- ProjetPOO/src/mypackage/Myclass.java
+++ ProjetPOO/src/mypackage/Myclass.java
@@ -3,7 +3,7 @@ package mypackage;
 public class Myclass {
 	
 	public static void main(String[] args){
-		Objet obj = new Objet(5,"yoho");
+		Objet obj = new Objet(5,"yohohi");
 		obj.multiply(10);
 		System.out.println("a="+obj.getA()+" and b="+obj.getB());
 		ObjectPlus obj2 = new ObjectPlus(3,"hey",'H');
